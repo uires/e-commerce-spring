e4a60f5189c5e92be3c5be3fbf6cba1f5f2179b5
uires <uiresdeivide1@gmail.com>
2018-08-21 15:16:53 -0300

    adicionando o handleResourcer

--- src/main/java/br/com/livrariaecommerce/configuration/AppWebConfiguration.java
+++ src/main/java/br/com/livrariaecommerce/configuration/AppWebConfiguration.java
@@ -3,19 +3,26 @@ package br.com.livrariaecommerce.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 import br.com.livrariaecommerce.controller.HomeController;
 
 @EnableWebMvc
 @ComponentScan(basePackageClasses = { HomeController.class })
-public class AppWebConfiguration {
+public class AppWebConfiguration extends WebMvcConfigurationSupport {
 
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resourceViewResolver = new InternalResourceViewResolver();
-		resourceViewResolver.setPrefix("WEB-INF/views");
+		resourceViewResolver.setPrefix("WEB-INF/view/");
 		resourceViewResolver.setSuffix(".jsp");
 		return resourceViewResolver;
+	}
+
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 }
