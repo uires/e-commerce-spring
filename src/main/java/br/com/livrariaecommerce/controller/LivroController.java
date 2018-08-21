34a28c12241df9d29f3b12ab227df09e4b782d7f
uires <uiresdeivide1@gmail.com>
2018-08-21 15:19:16 -0300

    injetando dependência no controlador(entity manager)

--- src/main/java/br/com/livrariaecommerce/controller/LivroController.java
+++ src/main/java/br/com/livrariaecommerce/controller/LivroController.java
@@ -1,20 +1,25 @@
 package br.com.livrariaecommerce.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import br.com.livrariaecommerce.dao.LivroDAO;
 import br.com.livrariaecommerce.entity.Livro;
 
 @Controller
 public class LivroController {
-	
+
+	@Autowired
+	private LivroDAO repository;
+
 	@RequestMapping("cadastroLivro")
 	public ModelAndView cadastroLivroMapping() {
 		return new ModelAndView("livro/livro-formulario").addObject("livro", new Livro());
 	}
-	
+
 	@RequestMapping("cadastrarLivro")
 	public ModelAndView e(@ModelAttribute("livro") Livro livro) {
 		System.out.println(livro.getDescricao());
