10c8fb23ecc5823005a97fb7bdaa719016f91a04
uires <uiresdeivide1@gmail.com>
2018-08-22 16:28:05 -0300

    configuração JPA e estabelecendo transicional

--- src/main/java/br/com/livrariaecommerce/configuration/JPAConfiguration.java
+++ src/main/java/br/com/livrariaecommerce/configuration/JPAConfiguration.java
@@ -2,35 +2,44 @@ package br.com.livrariaecommerce.configuration;
 
 import java.util.Properties;
 
+import javax.persistence.EntityManagerFactory;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
+@EnableTransactionManagement
 public class JPAConfiguration {
 
 	@Bean
-	public LocalContainerEntityManagerFactoryBean entityManagerFactoryGeneratterObject() {
-		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean 
-			= new LocalContainerEntityManagerFactoryBean();
+	public LocalContainerEntityManagerFactoryBean entityManager() {
+		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
 		JpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
 		localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
-		
+
 		DriverManagerDataSource driverManagerDataSource = new DriverManagerDataSource();
-		driverManagerDataSource.setPassword("");
 		driverManagerDataSource.setUsername("root");
+		driverManagerDataSource.setPassword("");
 		driverManagerDataSource.setUrl("jdbc:mysql://localhost:3306/livrariaecommerce");
-		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.DriverName");
+		driverManagerDataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		localContainerEntityManagerFactoryBean.setDataSource(driverManagerDataSource);
-		
+
 		Properties properties = new Properties();
 		properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
 		properties.setProperty("hibernate.show_sql", "true");
 		properties.setProperty("hibernate.hbm2ddl.auto", "update");
 		localContainerEntityManagerFactoryBean.setJpaProperties(properties);
-		
+
 		localContainerEntityManagerFactoryBean.setPackagesToScan("br.com.livrariaecommerce.entity");
 		return localContainerEntityManagerFactoryBean;
+	}
+
+	@Bean
+	public JpaTransactionManager transactionalManager(EntityManagerFactory entityManagerFactory) {
+		return new JpaTransactionManager(entityManagerFactory);
 	}
 }
