9aaf98b7d7995f07a940d48a13fc1a71e0dea681
uires <uiresdeivide1@gmail.com>
2018-08-22 16:24:02 -0300

    pequenas alterações na entidade livro

--- src/main/java/br/com/livrariaecommerce/entity/Livro.java
+++ src/main/java/br/com/livrariaecommerce/entity/Livro.java
@@ -1,8 +1,13 @@
 package br.com.livrariaecommerce.entity;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -11,15 +16,24 @@ import lombok.Setter;
 
 @Getter
 @Setter
-@Entity
-@Table(name = "livro")
 @NoArgsConstructor
 @AllArgsConstructor
+@Entity
+@Table(name = "loja")
 public class Livro {
 	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
+	@Column(name = "titulo", nullable = false, length = 155)
+	@NotNull(message = "Esse campo é obrigatório")
+	@Size(min = 5, max = 155, message = "Esse campo deve ter no mínino 5 caracteres e no máximo 155")
 	private String tituloDoLivro;
+	@Column(name = "descricao", nullable = false)
+	@NotNull(message = "Esse campo é obrigatório")
+	@Size(min = 5, message = "Esse campo deve ter no mínino 5 caracteres")
 	private String descricao;
+	@Column(name = "paginas", nullable = false)
+	@NotNull(message = "Esse campo é obrigatório")
 	private Integer paginas;
 
 }
