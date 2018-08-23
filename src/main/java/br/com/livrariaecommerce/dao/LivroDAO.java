da37fdf462d1841a3e8c1d68aa33ba1377d32c5e
uires <uiresdeivide1@gmail.com>
2018-08-23 01:26:05 -0300

    método para retornar lista de livros

--- src/main/java/br/com/livrariaecommerce/dao/LivroDAO.java
+++ src/main/java/br/com/livrariaecommerce/dao/LivroDAO.java
@@ -1,13 +1,17 @@
 package br.com.livrariaecommerce.dao;
 
+import java.util.List;
+
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import br.com.livrariaecommerce.entity.Livro;
 
 @Repository
+@Transactional
 public class LivroDAO {
 
 	@PersistenceContext
@@ -16,4 +20,9 @@ public class LivroDAO {
 	public void save(Livro livro) {
 		manager.persist(livro);
 	}
+
+	public List<Livro> selectAllLivros() {
+		return manager.createQuery("from Livro", Livro.class).getResultList();
+	}
+
 }
