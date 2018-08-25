2a0f80db1f14b2a3b2104a4ea4ba38631ce1ab76
uires <uiresdeivide1@gmail.com>
2018-08-24 23:42:58 -0300

    passando uma / no prefix do InternalResourcerViewResolver

--- src/main/java/br/com/livrariaecommerce/configuration/AppWebConfiguration.java
+++ src/main/java/br/com/livrariaecommerce/configuration/AppWebConfiguration.java
@@ -3,7 +3,6 @@ package br.com.livrariaecommerce.configuration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -17,13 +16,8 @@ public class AppWebConfiguration extends WebMvcConfigurationSupport {
 	@Bean
 	public InternalResourceViewResolver internalResourceViewResolver() {
 		InternalResourceViewResolver resourceViewResolver = new InternalResourceViewResolver();
-		resourceViewResolver.setPrefix("WEB-INF/view/");
+		resourceViewResolver.setPrefix("/WEB-INF/view/");
 		resourceViewResolver.setSuffix(".jsp");
 		return resourceViewResolver;
-	}
-
-	@Override
-	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-		registry.addResourceHandler("/resources/**").addResourceLocations("/resources/");
 	}
 }
