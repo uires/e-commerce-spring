652d4ea74130379dba0383da5bd5039957616416
uires <uiresdeivide1@gmail.com>
2018-08-24 23:43:56 -0300

    dando redirect para a lista e flashAtribute

--- src/main/java/br/com/livrariaecommerce/controller/LivroController.java
+++ src/main/java/br/com/livrariaecommerce/controller/LivroController.java
@@ -5,29 +5,33 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import br.com.livrariaecommerce.dao.LivroDAO;
 import br.com.livrariaecommerce.entity.Livro;
 
 @Controller
+@RequestMapping("/livro")
 public class LivroController {
 
 	@Autowired
 	private LivroDAO repository;
 
-	@RequestMapping("cadastroLivro")
+	@RequestMapping("/cadastroLivro")
 	public ModelAndView cadastroLivroMapping() {
 		return new ModelAndView("livro/livro-formulario").addObject("livro", new Livro());
 	}
 
 	@RequestMapping("cadastrarLivro")
-	public ModelAndView e(@ModelAttribute("livro") Livro livro) {
+	public ModelAndView e(@ModelAttribute("livro") Livro livro, RedirectAttributes redirectAttributes) {
 		repository.save(livro);
-		return cadastroLivroMapping();
+		redirectAttributes.addFlashAttribute("sucess", "Livro cadastrado com sucesso!");
+		return new ModelAndView("redirect:/listaLivros");
 	}
 
-	@RequestMapping("listaLivros")
+	@RequestMapping("/listaLivros")
 	public ModelAndView listaLivros() {
+		System.out.println("mapping \"/lista-livros\"");
 		return new ModelAndView("livro/livro-listagem").addObject("livros", repository.selectAllLivros());
 	}
 
