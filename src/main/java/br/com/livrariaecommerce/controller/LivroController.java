3cd234dbef1e832e9245270ec6cf10b636cb1849
uires <uiresdeivide1@gmail.com>
2018-09-02 12:18:16 -0300

    mapeando requisição e contruindo método busca

--- src/main/java/br/com/livrariaecommerce/controller/LivroController.java
+++ src/main/java/br/com/livrariaecommerce/controller/LivroController.java
@@ -1,9 +1,13 @@
 package br.com.livrariaecommerce.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BeanPropertyBindingResult;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -23,7 +27,11 @@ public class LivroController {
 	}
 
 	@RequestMapping("cadastrarLivro")
-	public ModelAndView e(@ModelAttribute("livro") Livro livro, RedirectAttributes redirectAttributes) {
+	public ModelAndView e(@ModelAttribute("livro") Livro livro, RedirectAttributes redirectAttributes,
+			BeanPropertyBindingResult beanPropertyBindingResult) {
+		if (beanPropertyBindingResult.hasErrors()) {
+			return new ModelAndView("livro/livro-formulario");
+		}
 		repository.save(livro);
 		redirectAttributes.addFlashAttribute("sucess", "Livro cadastrado com sucesso!");
 		return new ModelAndView("redirect:/listaLivros");
@@ -35,4 +43,13 @@ public class LivroController {
 		return new ModelAndView("livro/livro-listagem").addObject("livros", repository.selectAllLivros());
 	}
 
+	public ModelAndView buscaLivro(@RequestParam("filtre") String filtro, RedirectAttributes atributters) {
+		List<Livro> seachLivros = repository.seachLivros(filtro);
+		if ((seachLivros != null) && seachLivros.size() > 0) {
+			return new ModelAndView("livro/livro-listagem").addObject("livros", seachLivros);
+		} else {
+			atributters.addFlashAttribute("emptyResult", "Nenhum registro encontrado!");
+			return new ModelAndView("livro/livro-listagem");
+		}
+	}
 }
