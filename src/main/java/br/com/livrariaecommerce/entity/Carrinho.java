7814608d35218883392b5c7684021da0aefc2c48
uires <uiresdeivide1@gmail.com>
2018-09-16 21:46:03 -0300

    configurando o escopo do bean para session

--- src/main/java/br/com/livrariaecommerce/entity/Carrinho.java
+++ src/main/java/br/com/livrariaecommerce/entity/Carrinho.java
@@ -3,14 +3,17 @@ package br.com.livrariaecommerce.entity;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
+import org.springframework.web.context.WebApplicationContext;
 
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
-@Component
+@Component // configurando o bean do spring como session scope, padrão é singleton
+@Scope(value = WebApplicationContext.SCOPE_SESSION)
 public class Carrinho {
 	private Float valorTotal;
 	private List<Livro> livros = new ArrayList<>();
